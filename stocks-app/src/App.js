4e3c68b6642815f2408c3f2dc4cd9caf41e3e5af
ericb2020 <ebrauer83@gmail.com>
2021-02-18 16:37:14 -0600

    header complete

--- stocks-app/src/App.js
+++ stocks-app/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './Header';
+import Stocks from './Stocks';
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Header />
+      <Stocks />
     </div>
   );
 }
---
+++ stocks-app/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>STOCKS</header>
+  )
+}
+
+export default Header;
